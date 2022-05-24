0548adf90cdf4eadf71036528303f9ac941b8086
Andysmile101 <andreyturakin@gmail.com>
2022-05-24 06:52:40 +0200

    Added Stack

---
+++ Stack/Stack.py
@@ -0,0 +1,14 @@
+# I'm not sure this is how it will look like but this is my best guess
+list=[]
+list.append(1) # append 1
+print("push:",list)
+list.append(2) # append 2
+print("push:",list)
+list.append(3) # append 3
+print("push:",list)
+list.pop() # pop 3
+print("pop:",list)
+print("peek:",list[-1]) # get top most element
+list.pop() # pop 2
+print("pop:",list)
+print("peek:",list[-1]) # get top most element
