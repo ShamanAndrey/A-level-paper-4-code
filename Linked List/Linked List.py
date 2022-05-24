187c8d86d5adebfa8dd0748fd7d1f015fe2b5982
Andysmile101 <andreyturakin@gmail.com>
2022-05-24 06:32:41 +0200

    Linked List added

--- Linked List/Linked List.py
+++ Linked List/Linked List.py
@@ -1,8 +1,18 @@
-# NOT FINISHED
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
 
-class Node():
-    def __init__(self, Data, Next):
-        self.Data = Data
-        self.Next = Next
+class SLinkedList:
+   def __init__(self):
+      self.headval = None
 
-arr = [Node('asd',1),Node('ssd',2)]
+list1 = SLinkedList()
+list1.headval = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
+# Link first Node to second node
+list1.headval.nextval = e2
+
+# Link second Node to third node
+e2.nextval = e3
